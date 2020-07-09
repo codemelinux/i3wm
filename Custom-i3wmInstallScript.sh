88b1c4a20f5234038230a68bb45014bc36e3b56e
Benedict O. Ugbaja <codemelinux@gmail.com>
2020-07-09 05:56:20 -0700

    Update Custom-i3wmInstallScript.sh

--- Custom-i3wmInstallScript.sh
+++ Custom-i3wmInstallScript.sh
@@ -18,7 +18,7 @@ echo ""
 
 sudo apt install snapd
 
-sudo apt install -y i3-gaps i3lock python3-pip gedit ranger arandr nitrogen feh compton rofi variety
+sudo apt install -y i3-gaps i3lock python3-pip gedit ranger arandr nitrogen feh compton rofi variety polybar
 
 echo "###############################
 ###   Installing pywal   #####
@@ -36,17 +36,17 @@ echo "###############################
 echo ""
 echo "" 
 
-sudo apt-get install \
-  cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev \
-  libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev \
-  libxcb-util0-dev libxcb-xkb-dev pkg-config python3-xcbgen \
-  xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev \
-  libiw-dev libcurl4-openssl-dev libpulse-dev \
-  libxcb-composite0-dev xcb libxcb-ewmh2 libjsoncpp-dev
+#sudo apt-get install \
+  #cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev \
+  #libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev \
+  #libxcb-util0-dev libxcb-xkb-dev pkg-config python3-xcbgen \
+  #xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev \
+  #libiw-dev libcurl4-openssl-dev libpulse-dev \
+  #libxcb-composite0-dev xcb libxcb-ewmh2 libjsoncpp-dev
   
-cd ~/Downloads && git clone https://github.com/jaagr/polybar.git
+#cd ~/Downloads && git clone https://github.com/jaagr/polybar.git
 
-cd polybar && ./build.sh
+#cd polybar && ./build.sh
 
 echo ""
 echo "" 
@@ -73,5 +73,8 @@ cd ~/Downloads/i3wm && mv i3/ ~/.config/i3/
 cd ~/Downloads/i3wm && mv polybar/ ~/.config/polybar/
 cd ~/Downloads/i3wm && mv .bashrc ~/.bashrc
 cd ~/Downloads/i3wm && mv .fonts/ ~/.fonts/
+
+sudo chmod -R 774 ~/.config/polybar/
+sudo chmod -R 774 ~/.config/i3/
 
 
